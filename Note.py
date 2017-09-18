5bd1b6815f78d472e45118e042cbbc930a6a8d8e
David Saffo <dsaffo@luc.edu>
2017-09-18 15:18:30 -0500

    Basic .py file Note.py to start Command line interface

---
+++ Note.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 15:10:15 2017
+
+@author: dsaffo
+""" 
+
+def main():
+    print("Hello welcome to noterizerthingy!")
+    filepath = input("Please enter file path: ")
+    print(filepath)
+    
+    
+main()
